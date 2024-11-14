1d555291ed019990cca115ebb13558acc8a1acb1
samuelefr <samuele.francese@gmail.com>
2024-11-14 19:47:59 +0100

    secondo commit

--- slide1/es2.php
+++ slide1/es2.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>es2</title>
+    <title>Paolo</title>
 </head>
+
 <body>
+    <h1>Es2</h1>
     
+    <?php
+    $paolo = "Paolo";
+    $today = new DateTime("now", new DateTimeZone('Europe/Rome'));
+    echo $today->format('H:i:s');
+    $ora = $today->format('H');
+    echo "<br>\n Sono le $ora";
+    
+    if ($ora >= 18 || $ora < 0) {
+        echo " Buonasera $paolo";
+    } elseif ($ora >= 0 && $ora < 7) {
+        echo " Buonanotte $paolo";
+    } elseif ($ora >= 7 && $ora < 18) {
+        echo " Buongiorno $paolo";
+    }
+    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    echo "<br>\n Stai utilizzando il browser: ";
+    if (strpos($userAgent, 'Chrome') !== false) {
+        echo "Google Chrome";
+    } elseif (strpos($userAgent, 'Firefox') !== false) {
+        echo "Mozilla Firefox";
+    } elseif (strpos($userAgent, 'Safari') !== false && strpos($userAgent, 'Chrome') === false) {
+        echo "Safari";
+    } elseif (strpos($userAgent, 'Edge') !== false) {
+        echo "Microsoft Edge";
+    } elseif (strpos($userAgent, 'Opera') !== false || strpos($userAgent, 'OPR') !== false) {
+        echo "Opera";
+    } else {
+        echo "un browser sconosciuto";
+    }
+    ?>
+    <br>
+    <a href="slide1.html">torna indietro</a><br> 
 </body>
 </html>
---
+++ slide1/es3.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Triangolini</title>
+    <style>body {font-family: monospace;}</style>
+</head>
+    
+    <h1>Triangolini</h1>
+    
+    <?php
+    
+    //a
+    for($i=0; $i<11; $i++){
+        for($c=0;$c<$i;$c++){
+            echo "*";
+        }
+        echo "<br>";
+    }
+    echo "<br><br><br>";
+    //b
+    for($i=11;$i>0;$i--){
+        for($c=0;$c<$i;$c++){
+            echo "*";
+        }
+        echo "<br>";
+    }
+    echo "<br><br><br>";
+    //c
+    for($i=0;$i<11;$i++){
+        for($c=0;$c<$i;$c++){
+            echo "&nbsp;";
+            
+        }
+        for($i=11;$i>$c;$i--){
+        echo "*";
+        }
+        echo "<br>";
+    }
+    echo "<br><br><br>";
+    //d
+    for($i=0;$i<11;$i++){
+        for($c=11;$c>$i;$c--){
+            echo "&nbsp;";
+            
+        }
+        for($i=0;$i<$c;$i++){
+        echo "*";
+        }
+        echo "<br>";
+    }
+    echo "<br><br><br>";
+    ?>
+    <br>
+    <a href="slide1.html">torna indietro</a><br> 
+
+</html>
